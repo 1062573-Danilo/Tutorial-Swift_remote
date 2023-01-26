3a1987bf8b3937200a06dd442a445727acfb6008
DaniloC <1062573@hr.nl>
2023-01-26 16:43:39 +0100

    functions are working properly

---
+++ Tutorial Swift/Views/LandmarkList.swift..swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkList.swift..swift
+//  Tutorial Swift
+//
+//  Created by Danilo Chapper on 26/01/2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
