7405b71e82f88bca17499ef4b689a6e9661ad9ae
DaniloC <1062573@hr.nl>
2023-01-26 10:06:21 +0100

    Initial Commit

---
+++ Tutorial Swift/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Tutorial Swift
+//
+//  Created by Danilo Chapper on 25/01/2023.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ Tutorial Swift/Tutorial_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial_SwiftApp.swift
+//  Tutorial Swift
+//
+//  Created by Danilo Chapper on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
